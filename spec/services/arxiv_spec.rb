54185744674200419a2ad5ba992b27472c3e92fe
Calvin Conley <cconley0125@gmail.com>
2021-06-10 15:58:20 -0400

    added tests for Arxiv.rb

--- spec/services/arxiv_spec.rb
+++ spec/services/arxiv_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper.rb'
 
 describe Arxiv do
-  describe "::next_ten" do
+  describe "::get_ten_articles", :vcr do
     it "returns array of hashes" do
-      response = Arxiv.get_ten_articles(["gene editing"])
-      expect(response[0].class).to eq(Hash)
+      articles = Arxiv.get_ten_articles(["gene editing"], 0)
+      expect(articles[0].class).to eq(Hash)
+    end
+    
+    it "returns empty array if no results found" do
+      articles = Arxiv.get_ten_articles(["qwefqweqefqwfwew"], 0)
+      expect(articles.length).to eq(0)
     end
 
-    it "doesn't retrieve any ignored id's" do
-      
+    it "returns array of hashes that can be used to create a new article after associating them with a topic" do 
+      articles = Arxiv.get_ten_articles(["synthetic biology"], 0)
+      articles[0][:research_topic_id] = Fabricate(:research_topic).id
+      ResearchArticle.create(articles[0])
+      expect(ResearchArticle.count).to eq(1)
     end
   end
 end
