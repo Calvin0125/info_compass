a8eef62b6961cc005883654767cb051cde6def5f
Calvin Conley <cconley0125@gmail.com>
2021-06-17 09:35:55 -0400

    updated routes.rb with users, sessions, articles, and topics

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-	resources :research_articles
+	resources :research_articles, only: [:update]
+  resources :research_topics, only: [:create, :update, :index, :destroy]
+  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :sessions, only: [:new, :create, :destroy]
 end
