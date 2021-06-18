e3d0edc41a306cd7fb8bf14fd2da511f393a5d32
Calvin Conley <cconley0125@gmail.com>
2021-06-17 20:44:26 -0400

    updated routes.rb for login

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 	resources :research_articles, only: [:update]
   resources :research_topics, only: [:create, :update, :index, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:create, :destroy]
+  get '/login', to: 'sessions#new', as: 'login'
 end
