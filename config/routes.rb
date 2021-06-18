5306f302312d3654ef7b5fb3a1bfc3a693f2b6eb
Calvin Conley <cconley0125@gmail.com>
2021-06-17 21:52:10 -0400

    created new_user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 	resources :research_articles, only: [:update]
   resources :research_topics, only: [:create, :update, :index, :destroy]
-  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :users, only: [:create, :show, :edit, :update]
+  get '/new_user', to: 'users#new', as: 'new_user'
   resources :sessions, only: [:create, :destroy]
   get '/login', to: 'sessions#new', as: 'login'
 end
