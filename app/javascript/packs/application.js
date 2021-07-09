bb641929d81bbc0adeb91c1160107929ff50b04b
Calvin Conley <cconley0125@gmail.com>
2021-07-08 20:53:57 -0400

    finished the new research topics index design for now

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../stylesheets/application'
 import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
