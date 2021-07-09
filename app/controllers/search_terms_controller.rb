8d53905430ce0bbc72326310332cee28c4c1ab38
Calvin Conley <cconley0125@gmail.com>
2021-07-09 16:01:38 -0400

    updated search terms delete action to refresh new articles

--- app/controllers/search_terms_controller.rb
+++ app/controllers/search_terms_controller.rb
@@ -1,12 +1,13 @@
 class SearchTermsController < ApplicationController
   def destroy
     term = SearchTerm.find(params[:id])
-    if term.research_topic.user == helpers.current_user
+    topic = term.research_topic
+    if topic.user == helpers.current_user
       term.destroy
+      topic.refresh_new_articles
     else
       flash[:danger] = "You can only delete search terms for topics that belong to you."
     end
-
     redirect_to research_topics_path
   end
 end
--- spec/controllers/search_terms_controller_spec.rb
+++ spec/controllers/search_terms_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper.rb'
 
 describe SearchTermsController do
   describe "DELETE destroy" do
-    it "removes the specified search term" do
+    it "removes the specified search term", :vcr do
       user = Fabricate(:user)
       login(user)
       topic = Fabricate(:research_topic, user_id: user.id)
@@ -11,7 +11,7 @@ describe SearchTermsController do
       expect(SearchTerm.count).to eq(0)     
     end
 
-    it "only removes the search term if it belongs to the logged in user" do
+    it "only removes the search term if it belongs to the logged in user", :vcr do
       user1 = Fabricate(:user)
       login(user1)
       user2 = Fabricate(:user, id: 2)
@@ -21,7 +21,7 @@ describe SearchTermsController do
       expect(SearchTerm.count).to eq(1)
     end
 
-    it "sets the flash warning if user tries to delete a term for a topic that isn't theirs" do
+    it "sets the flash warning if user tries to delete a term for a topic that isn't theirs", :vcr do
       user1 = Fabricate(:user)
       login(user1)
       user2 = Fabricate(:user, id: 2)
@@ -31,13 +31,23 @@ describe SearchTermsController do
       expect(flash[:danger]).to eq("You can only delete search terms for topics that belong to you.")
     end
 
-    it "redirects to the topics index page" do
+    it "redirects to the topics index page", :vcr do
       user = Fabricate(:user)
       login(user)
       topic = Fabricate(:research_topic, user_id: user.id)
       term = Fabricate(:search_term, research_topic_id: topic.id)
       delete :destroy, params: { id: term.id.to_s }
       expect(response).to redirect_to(research_topics_path)
+    end
+
+    it "calls #refresh_new_articles on the associated topic", :vcr do
+      user = Fabricate(:user)
+      login(user)
+      topic = Fabricate(:research_topic, user_id: user.id, id: 1)
+      term1 = Fabricate(:search_term, research_topic_id: 1, term: 'crispr')
+      term2 = Fabricate(:search_term, research_topic_id: 1, id: 2)
+      delete :destroy, params: { id: term2.id.to_s }
+      expect(ResearchArticle.count).to eq(10)
     end
   end
 end
