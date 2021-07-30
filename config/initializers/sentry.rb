e27ff1fc17dbc94aa35b731715f889efbcd9f770
Calvin Conley <cconley0125@gmail.com>
2021-07-30 14:02:40 -0400

    change to sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,4 @@ Sentry.init do |config|
   config.dsn = 'https://1eaab729f1c24d65b8134c37a0daf77e@o936036.ingest.sentry.io/5886111'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.traces_sample_rate = 0.5
-  config.traces_sampler = lambda do |context|
-    true
-  end
 end
