ccf9fcad02f32f14f1e5b3c1b76a00f0412ff420
Calvin Conley <cconley0125@gmail.com>
2021-08-14 08:38:35 -0400

    updated default from in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'calvin@infocompass.tech'
+  default from: 'Info Compass <calvin@infocompass.tech>'
 
   def reset_password(user, url)
     @user, @url = user, url
