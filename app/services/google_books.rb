a45ec8b45a1d30260f2f11ef51f276e931e8eb05
Calvin Conley <cconley0125@gmail.com>
2021-11-13 12:03:03 -0500

    GoogleBooks class creates hash with unique titles that have oldest publishing date

--- app/services/google_books.rb
+++ app/services/google_books.rb
@@ -14,11 +14,12 @@ class GoogleBooks
 
   def self.get_all_formatted_results(author)
     start_index = 0
-    processed_books = {} 
+    processed_books = {}
     total_items_count = get_total_items_count(author)
 
     while start_index < total_items_count
       books = get_books_from_next_page(author, start_index)
+      break if !books
       processed_books = filter_results(books, processed_books, author)
       start_index += MAX_RESULTS_ALLOWED
     end
@@ -40,8 +41,8 @@ class GoogleBooks
 
   def self.filter_results(books, processed_books, author)
     books.each do |book|
-      if (book["volumeInfo"]["language"] == "en") &&
-         (one_author_matches(book["volumeInfo"]["authors"], author))
+      book_authors = book["volumeInfo"]["authors"]
+      if valid_book(book, processed_books, book_authors, author)
         title = book["volumeInfo"]["title"]
         published_date = book["volumeInfo"]["publishedDate"]
         authors = book["volumeInfo"]["authors"]
@@ -52,6 +53,21 @@ class GoogleBooks
     processed_books
   end
 
+  def self.valid_book(book, processed_books, book_authors, author_entered_by_user)
+    oldest_published_date_for_title(book, processed_books) &&
+    book["volumeInfo"]["language"] == "en" &&
+    book["saleInfo"]["country"] == "US" &&
+    one_author_matches(book_authors, author_entered_by_user)
+  end
+
+  def self.oldest_published_date_for_title(book, processed_books)
+    title = book["volumeInfo"]["title"]
+    return true if !processed_books[title]
+    already_added_title_date = Date.parse(processed_books[title][:published_date])
+    current_title_date = Date.parse(book["volumeInfo"]["publishedDate"])
+    current_title_date < already_added_title_date
+  end
+
   def self.one_author_matches(book_authors, author_entered_by_user)
     return false if !book_authors
     book_authors.each do |author|
@@ -60,3 +76,4 @@ class GoogleBooks
     false
   end
 end
+
