d41fac2a69b628ccd7e266b3d84927e21ce0b074
Calvin Conley <cconley0125@gmail.com>
2021-11-14 16:54:04 -0500

    GoogleBooks now returns valid books in convenient hash format

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # README
 
+Google Books Explanation
+  While playing around with the data, I found the Google Books API to
+  have an enormous amount of duplicates, missing metadata, and otherwise
+  things that I did not want included on the website. There was not a
+  perfect way to filter all of them out, but here is what I did to get 
+  highest quality data:
+    1. Only retrieve books that have a yyyy-mm-dd format for the date
+    2. Only retrieve books that were published in the U.S. in English
+    3. Downcase the title to avoid duplicates with different cases and
+       pick the title with the oldest publishing date
+  Unfortunately there is still a problem with different editions, but
+  filtering out books with the word 'edition' would eliminate valid
+  books with 'edition' in the title, and filtering out books with
+  similar titles but extra words on the end would eliminate a lot
+  of valid sequels.
+
+
+* I will fill out all the stuff for the readme later
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- app/services/google_books.rb
+++ app/services/google_books.rb
@@ -28,22 +28,22 @@ class GoogleBooks
   end
 
   def self.get_total_items_count(author)
-    response = Faraday.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:#{author}")
+    response = Faraday.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:#{author}&key=#{API_KEY}")
     books = JSON.parse(response.body)
     books["totalItems"]
   end
 
   def self.get_books_from_next_page(author, start_index)
-    response = Faraday.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:#{author}&startIndex=#{start_index}&maxResults=#{MAX_RESULTS_ALLOWED}")
+    response = Faraday.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:#{author}&startIndex=#{start_index}&maxResults=#{MAX_RESULTS_ALLOWED}&key=#{API_KEY}")
     books = JSON.parse(response.body)
     books["items"]
   end
 
   def self.filter_results(books, processed_books, author)
     books.each do |book|
-      book_authors = book["volumeInfo"]["authors"]
-      if valid_book(book, processed_books, book_authors, author)
-        title = book["volumeInfo"]["title"]
+      if valid_book(book, processed_books, author)
+        # title is downcased to avoid duplicates with different casing
+        title = book["volumeInfo"]["title"].downcase
         published_date = book["volumeInfo"]["publishedDate"]
         authors = book["volumeInfo"]["authors"]
         processed_books[title] = { published_date: published_date, authors: authors }
@@ -53,16 +53,23 @@ class GoogleBooks
     processed_books
   end
 
-  def self.valid_book(book, processed_books, book_authors, author_entered_by_user)
+  def self.valid_book(book, processed_books, author_entered_by_user)
+    book["volumeInfo"]["title"] &&
+    valid_date(book["volumeInfo"]["publishedDate"]) &&
     oldest_published_date_for_title(book, processed_books) &&
     book["volumeInfo"]["language"] == "en" &&
     book["saleInfo"]["country"] == "US" &&
-    one_author_matches(book_authors, author_entered_by_user)
+    one_author_matches(book["volumeInfo"]["authors"], author_entered_by_user)
+  end
+
+  def self.valid_date(date)
+    /\A\d{4}-\d{2}-\d{2}\Z/.match?(date)
   end
 
   def self.oldest_published_date_for_title(book, processed_books)
-    title = book["volumeInfo"]["title"]
+    title = book["volumeInfo"]["title"].downcase
     return true if !processed_books[title]
+
     already_added_title_date = Date.parse(processed_books[title][:published_date])
     current_title_date = Date.parse(book["volumeInfo"]["publishedDate"])
     current_title_date < already_added_title_date
@@ -76,4 +83,3 @@ class GoogleBooks
     false
   end
 end
-
