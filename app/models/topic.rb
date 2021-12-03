899229f198783ec07810202916a8ed53914d1fe1
Calvin Conley <cconley0125@gmail.com>
2021-12-03 16:58:23 -0500

    updated topic to get todays research articles up to 50

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -15,13 +15,14 @@ class Topic < ActiveRecord::Base
     end
   end
 
-  def self.add_new_articles
-    self.all.each do |topic|
+  def self.add_new_research_articles
+    self.where(category: "research").each do |topic|
       topic.add_new_articles
     end
   end
   
   def refresh_new_articles
+    # used when a user adds or deletes a search term
     self.articles.where(status: "new").each do |article|
       article.destroy
     end
@@ -30,37 +31,73 @@ class Topic < ActiveRecord::Base
   
   def add_new_articles
     search_terms = self.search_terms.map(&:term)
-    page = 0
+    return if search_terms == []
 
-    # loop ensures that newly published articles always get added, but
-    # if there are not enough newly published articles to fill the 'new'
-    # section with 10 articles, it will look for older and older articles
-    # to find 10 the user hasn't seen before
-    loop do
+    self.get_todays_articles_up_to_50(search_terms)
+    self.ensure_at_least_10_articles(search_terms)
+    # if user did not read or save articles added yesterday
+    # they will be replaced by the new articles added today
+    self.remove_old_articles_marked_new
+  end
+
+  def remove_old_articles_marked_new
+    while self.articles.where(status: "new").length > 10
+      article = self.articles.where(status: "new").except(:order).order(date_published: :asc).first
+      break if published_yesterday(article)
+      article.destroy
+    end
+  end
+
+  def ensure_at_least_10_articles(search_terms)
+    page = 0
+    while self.articles.where(status: "new").length < 10    
       articles = self.process_next_ten_articles(search_terms, page)
-      break if self.articles.where(status: "new").length > 10 || articles.length < 10
+      break if self.articles.where(status: "new").length >= 10 ||
+               articles.length < 10
       page += 1
     end
-    self.ensure_only_ten_new_articles
+  end
+
+  def get_todays_articles_up_to_50(search_terms)
+    page = 0
+    loop do
+      articles = Arxiv.get_ten_articles(search_terms, page)
+      articles.each do |article|
+        if published_yesterday(article) && unique(article) &&
+           self.research_new_today_count < 50 
+          article[:topic_id] = self.id
+          Article.create(article)
+        end
+      end
+
+      if !published_yesterday(articles[9]) || articles.length < 10 ||
+         self.research_new_today_count >= 50 
+        break
+      end
+      page += 1
+    end
+  end
+
+  def published_yesterday(article)
+     article[:date_published] == Date.yesterday.strftime("%F") ||
+     article[:date_published] == Date.yesterday
+  end
+
+  def unique(article)
+    self.articles.where(api: article[:api], url: article[:url]).length == 0
   end
 
   def process_next_ten_articles(search_terms, page)
     articles = Arxiv.get_ten_articles(search_terms, page)
       articles.each do |article|
-        unless self.articles.where(api: article[:api], url: article[:url]).length > 0
+        if unique(article)
           article[:topic_id] = self.id
           Article.create(article)
         end
       end
   end
 
-  def ensure_only_ten_new_articles
-    while self.articles.where(status: "new").length > 10
-      self.articles.where(status: "new").except(:order).order(date_published: :asc).first.destroy
-    end
-  end
-
-  def new_today_count
+  def research_new_today_count
     return self.articles.where(date_published: Date.yesterday.strftime("%F"), status: "new").length
   end
 
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -72,19 +72,19 @@ describe Topic do
       topic = Fabricate(:topic, user_id: Fabricate(:user).id)
       Fabricate(:search_term, topic_id: topic.id, term: "artificial intelligence")
       Topic.add_new_articles
-      expect(Article.count).to eq(10)
+      expect(Article.count).to be >= 10
     end
     
-    it "loads 10 new articles for each topic" do
+    it "loads at least 10 new articles for each topic" do
       topic1 = Fabricate(:topic, user_id: Fabricate(:user).id)
       topic2 = Fabricate(:topic, id: 2, user_id: Fabricate(:user).id)
       Fabricate(:search_term, topic_id: topic1.id, id: 1, term: "artificial intelligence")
       Fabricate(:search_term, topic_id: topic2.id, id: 2, term: "synthetic biology")
       Topic.add_new_articles
-      expect(Article.count).to eq(20)
+      expect(Article.count).to be >= 20
     end
 
-    it "removes oldest articles marked new to ensure only 10 articles marked new in db at once" do
+    it "removes oldest articles marked new to ensure at most 50 articles marked new in db at once" do
       topic = Fabricate(:topic, user_id: Fabricate(:user).id)
       3.times {|n| Fabricate(:article, date_published: '2021-01-01', topic_id: topic.id, id: n + 1) }
       Fabricate(:search_term, topic_id: topic.id, term: "artificial intelligence")
@@ -93,25 +93,29 @@ describe Topic do
       expect(Article.where(id: 2).length).to eq(0)
     end
 
-    it "will request multiple pages until there are 10 new articles" do
+    it "will request multiple pages until there are at least 10 new articles" do
       topic = Fabricate(:topic, user_id: Fabricate(:user).id)
+      articles = Arxiv.get_ten_articles(["black holes"], 0)
+      url0 = articles[0][:url]
+      url1 = articles[1][:url]
       Fabricate(:article, topic_id: topic.id, api: 'arxiv', 
-      url: 'http://arxiv.org/abs/2106.05259v1', status: "read")
+      url: url0, status: "read")
       Fabricate(:article, topic_id: topic.id, api: 'arxiv',
-      url: 'http://arxiv.org/abs/2106.05228v1', status: "read", id: 2)
+      url: url1, status: "read", id: 2)
       Fabricate(:search_term, topic_id: topic.id, term: "black holes")
       Topic.add_new_articles
-      expect(Article.count).to eq(12)
+      expect(Article.count).to be >= 12
     end
 
     it "won't add the same article twice" do
       topic = Fabricate(:topic, user_id: Fabricate(:user).id)
-      api_id = 'http://arxiv.org/abs/2106.05259v1'
+      article = Arxiv.get_ten_articles(["black holes"], 0)[0]
+      url = article[:url]
       Fabricate(:article, topic_id: topic.id, api: 'arxiv', 
-      url: api_id, status: "read")
+      url: url, status: "read")
       Fabricate(:search_term, topic_id: topic.id, term: "black holes")
       Topic.add_new_articles
-      expect(topic.articles.where(api: 'arxiv', url: api_id).length).to eq(1) 
+      expect(topic.articles.where(api: 'arxiv', url: url).length).to eq(1) 
     end
   end
 
