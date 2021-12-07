f585c0ea5ef970ab51f954c0798cf1b1b2e5c0fe
Calvin Conley <cconley0125@gmail.com>
2021-12-07 17:33:11 -0500

    added #pretty_time_published method to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,10 @@ class Article < ActiveRecord::Base
   default_scope { order('date_published DESC, time_published DESC, title') }
 
   validates_presence_of :title, :author_csv, :summary, :api, :url, :date_published
+
+  def pretty_time_published(time_zone)
+    return if !self.time_published
+
+    self.time_published.in_time_zone(time_zone).strftime("%l:%M %p").strip
+  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -31,4 +31,30 @@ describe Article do
      expect(topic.articles.all.order('date_published DESC, time_published DESC, title')).to eq(topic.articles.all)
    end
  end
+
+ describe "#pretty_time_published" do
+   before :each do
+     @user = Fabricate(:user)
+     @topic = Fabricate(:topic, user_id: @user.id)
+   end
+
+   it "returns nil if there is no time published (ie. for research articles)" do
+     article = Fabricate(:article, topic_id: @topic.id)  
+     expect(article.pretty_time_published("Mountain Time (US & Canada)")).to eq(nil)
+   end
+
+   it "returns pretty time in correct time zone" do
+     article = Fabricate(:article, topic_id: @topic.id, time_published: "2021-12-07T21:25:27+00:00")
+     # Must use in_time_zone because manually entering the time as a string
+     # will cause tests to fail during daylight savings time
+     correct_time = Time.parse("21:25 UTC").in_time_zone("Eastern Time (US & Canada)").strftime("%l:%M %p").strip
+     expect(article.pretty_time_published("Eastern Time (US & Canada)")).to eq(correct_time)
+   end
+
+   it "returns another pretty time in correct time zone" do
+     article = Fabricate(:article, topic_id: @topic.id, time_published: "2021-12-07T11:50:35+00:00")
+     correct_time = Time.parse("11:50 UTC").in_time_zone("Pacific Time (US & Canada)").strftime("%l:%M %p").strip
+     expect(article.pretty_time_published("Pacific Time (US & Canada)")).to eq(correct_time)
+   end
+ end
 end
